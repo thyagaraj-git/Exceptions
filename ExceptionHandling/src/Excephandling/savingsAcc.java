d0cf7dbe1e8e0734c77f395574b41a827e9c630d
thyagaraj-git <93278675+thyagaraj-git@users.noreply.github.com>
2021-11-16 18:03:02 +0530

    Update savingsAcc.java

--- ExceptionHandling/src/Excephandling/savingsAcc.java
+++ ExceptionHandling/src/Excephandling/savingsAcc.java
@@ -5,21 +5,29 @@ import java.util.Scanner;
 public class savingsAcc {
 	public static void main(String args[])throws IllegalBankTransactionException,InsufficientBalanceException
 	{
-		double balance=10000;
-		
 		Scanner sc=new Scanner(System.in);
-		System.out.println("enter amount to be withdrawn");
-		double amount=sc.nextInt();
-		sc.close();
-		 double withdraw(amount);
-
-	private static void withdraw(double amount) throws IllegalBankTransactionException,InsufficientBalanceException{
-			if(amount<0){
-		throw new IllegalBankTransactionException();
-			}
-			else if(amount>balance) {
-				throw new InsufficientBalanceEception();
+		System.out.println("enter deposit amount");
+		double deposit=sc.nextDouble();
+		double balance =deposit;
+		
+		System.out.println("enter your withdraw amount");
+		double withdraw=sc.nextDouble();
+		withdraw(withdraw, balance);
 	}
+	
+		public static void withdraw(double withdraw,double balance) throws IllegalBankTransactionException,InsufficientBalanceException
+		{
+			if(withdraw<=balance && withdraw>0) {
+				System.out.println("amount= "+balance);
+				
+		}
+			else if(withdraw<0) {
+				throw new IllegalBankTransactionException();
+				
+			}
+			else if(withdraw>balance || withdraw==0) {
+				throw new InsufficientBalanceException();
+			}
 		
 	}
 }
